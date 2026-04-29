e20a27531a1e655f8e5ba7f934945be98dced39e
Ashish Trivedi <atrivedi@tsavoritesi.com>
2026-04-29 10:45:43 -0700

    @FIR-1763: Fixed the compilation issues

--- ggml/src/ggml-tsavorite/ggml-tsavorite.cpp
+++ ggml/src/ggml-tsavorite/ggml-tsavorite.cpp
@@ -3297,28 +3297,6 @@ static void ggml_backend_tsavorite_buffer_get_tensor(ggml_backend_buffer_t buffe
 }
 
 
-static void ggml_backend_tsavorite_buffer_set_tensor2(ggml_backend_buffer_t buffer,
-                                                     struct ggml_tensor *tensor, const void *data,
-                                                     size_t offset, size_t size, size_t n_copies,
-						     size_t stride_tensor, size_t stride_data) {
-  GGML_TSAVORITE_LOG_INFO("Start %s\n", __func__);
-  memcpy((char *)tensor->data + offset, data, size);
-  GGML_TSAVORITE_LOG_INFO("End %s\n", __func__);
-
-  TSI_UNUSED(buffer);
-}
-
-static void ggml_backend_tsavorite_buffer_get_tensor2(ggml_backend_buffer_t buffer,
-                                                     const struct ggml_tensor *tensor, void *data,
-                                                     size_t offset, size_t size, size_t n_copies,
-						     size_t stride_tensor, size_t stride_data) {
-  GGML_TSAVORITE_LOG_INFO("Start %s\n", __func__);
-  memcpy(data, (const char *)tensor->data + offset, size);
-  GGML_TSAVORITE_LOG_INFO("End %s\n", __func__);
-
-  TSI_UNUSED(buffer);
-}
-
 static bool ggml_backend_tsavorite_buffer_cpy_tensor(ggml_backend_buffer_t buffer,
                                                      const struct ggml_tensor *src,
                                                      struct ggml_tensor *dst) {
@@ -3352,8 +3330,8 @@ static struct ggml_backend_buffer_i ggml_backend_tsavorite_buffer_i = {
     /* .memset_tensor   = */ ggml_backend_tsavorite_buffer_memset_tensor,
     /* .set_tensor      = */ ggml_backend_tsavorite_buffer_set_tensor,
     /* .get_tensor      = */ ggml_backend_tsavorite_buffer_get_tensor,
-    /* .set_tensor2      = */ ggml_backend_tsavorite_buffer_set_tensor2,
-    /* .get_tensor2      = */ ggml_backend_tsavorite_buffer_get_tensor2,
+    /* .set_tensor_2d   = */ NULL,
+    /* .get_tensor_2d   = */ NULL,
     /* .cpy_tensor      = */ ggml_backend_tsavorite_buffer_cpy_tensor,
     /* .clear           = */ ggml_backend_tsavorite_buffer_clear,
     /* .reset           = */ NULL,
@@ -3625,8 +3603,8 @@ static struct ggml_backend_i ggml_backend_tsavorite_i = {
     /* .free                    = */ ggml_backend_tsavorite_free,
     /* .set_tensor_async        = */ NULL,
     /* .get_tensor_async        = */ NULL,
-    /* .set_tensor_async        = */ NULL,
-    /* .get_tensor_async        = */ NULL,
+    /* .set_tensor_2d_async     = */ NULL,
+    /* .get_tensor_2d_async     = */ NULL,
     /* .cpy_tensor_async        = */ NULL,
     /* .synchronize             = */ ggml_backend_tsavorite_synchronize,
     /* .graph_plan_create       = */ NULL,
